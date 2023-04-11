47ac283ad9b13fb8387b1775a4c396d39d99280e
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-04-11 20:41:34 +0800

    Setup App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+import Button from "../src/components/button/Button";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button label="2 Hours" onClick={() => {}} />
+      <Button label="24 Hours" onClick={() => {}} />
+      <Button label="4 days" onClick={() => {}} />
+    </div>
+  );
 }
 
 export default App;
