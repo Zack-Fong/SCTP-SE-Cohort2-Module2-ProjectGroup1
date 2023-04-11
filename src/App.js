997e6103ac94cb5c6d86e7ba268ba36007fc4d16
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-04-11 20:48:01 +0800

    Add Default Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Button from "../src/components/button/Button";
 import "./App.css";
+
+const DefaultPage = () => <p>Nothing to see here!</p>;
 
 function App() {
   return (
     <div className="App">
-      <Button label="2 Hours" onClick={() => {}} />
-      <Button label="24 Hours" onClick={() => {}} />
-      <Button label="4 days" onClick={() => {}} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Button label="2 Hours" onClick={() => {}} />
+                <Button label="24 Hours" onClick={() => {}} />
+                <Button label="4 days" onClick={() => {}} />
+              </>
+            }
+          />
+          <Route path="*" element={<DefaultPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
