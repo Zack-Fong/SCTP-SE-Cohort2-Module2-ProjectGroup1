a1f517c2d5faa1361e153e7aab8c4d8c6f3cd07e
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-04-19 07:03:15 +0800

    Revert FourDayScreen Changes

--- src/components/table/TableGeneral.js
+++ src/components/table/TableGeneral.js
@@ -1,39 +1,40 @@
 import tableStyles from "./Table.module.css";
+import { v4 as uuidv4 } from "uuid";
 
-function TableGeneral({generalForecast, generalHeaders}) {
-    return (
-        <table className={tableStyles.table}>
-        <thead>
-          <tr>
-            {generalHeaders.map((item) => {
-              return <th>{item}</th>
-            })}
-          </tr>
-        </thead>
-        <tbody>
+function TableGeneral({ generalForecast, generalHeaders }) {
+  return (
+    <table className={tableStyles.table}>
+      <thead>
+        <tr>
+          {generalHeaders.map((item) => {
+            return <th>{item}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
         {generalForecast &&
           generalForecast.map((generalForecast) => (
-            <tr>
+            <tr key={uuidv4()}>
               {generalForecast.date && <td>{generalForecast.date}</td>}
-            <td>{generalForecast.forecast}</td>
-            <td>
-              {generalForecast.relative_humidity_low} :{" "}
-              {generalForecast.relative_humidity_high}
-            </td>
-            <td>
-              {generalForecast.temperature_low} :{" "}
-              {generalForecast.temperature_high}
-            </td>
-            <td>
-              {generalForecast.wind_speed_low} :{" "}
-              {generalForecast.wind_speed_high}
-            </td>
-            <td>{generalForecast.wind_direction}</td>
-          </tr>
+              <td>{generalForecast.forecast}</td>
+              <td>
+                {generalForecast.relative_humidity_low} :{" "}
+                {generalForecast.relative_humidity_high}
+              </td>
+              <td>
+                {generalForecast.temperature_low} :{" "}
+                {generalForecast.temperature_high}
+              </td>
+              <td>
+                {generalForecast.wind_speed_low} :{" "}
+                {generalForecast.wind_speed_high}
+              </td>
+              <td>{generalForecast.wind_direction}</td>
+            </tr>
           ))}
-        </tbody>
-      </table>
-    )
+      </tbody>
+    </table>
+  );
 }
 
 export default TableGeneral;
